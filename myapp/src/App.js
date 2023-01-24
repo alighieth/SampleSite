0a25b67258f099362b4951ceb6ff1357641d3bfc
alighieth <alighieth2709@gmail.com>
2023-01-24 08:04:35 +0200

    speed dial for scrolling up

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,8 +4,14 @@ import About, { Extras } from "./glint/features";
 import General from "./glint/general";
 import Header from "./glint/header";
 import Skills from "./glint/skills";
-
+import SpeedDial from "@mui/material/SpeedDial";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
 // iphone 11 414 * 896
+
+const actions = [{ icon: <FileCopyIcon />, name: "Copy" }];
+
 function App() {
   return (
     <>
@@ -16,6 +22,20 @@ function App() {
         <Extras />
         <Skills />
         <Contact />
+        <SpeedDial
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+          style={{ position: "fixed" }}
+          ariaLabel="SpeedDial basic example"
+          sx={{
+            position: "absolute",
+            bottom: 16,
+            right: 16,
+            "& .MuiFab-primary": { backgroundColor: "#39b54a", color: "white" },
+          }}
+          icon={<ArrowUpwardIcon />}
+        ></SpeedDial>
       </div>
     </>
   );
